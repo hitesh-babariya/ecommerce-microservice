d7aa0d9b187dce7be214a18fc9fdb8e783ce7de3
hitesh-babariya <babariyahiteshb@gmail.com>
2026-09-15 12:02:51 +0530

    versionning added

--- user-service/internal/handler/user_handler.go
+++ user-service/internal/handler/user_handler.go
@@ -20,7 +20,8 @@ func NewUserHandler(userUsecase *usecase.UserUsecase) *UserHandler {
 	}
 }
 
-func (h *UserHandler) CreateUser(c *gin.Context) {
+// Versioning For Old Client V1 (http://127.0.0.1:8080/api/v1/users)
+func (h *UserHandler) CreateUserV1(c *gin.Context) {
 	var request model.CreateUserRequest
 
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -49,7 +50,7 @@ func (h *UserHandler) CreateUser(c *gin.Context) {
 
 }
 
-func (h *UserHandler) GetUserByID(c *gin.Context) {
+func (h *UserHandler) GetUserByIDV1(c *gin.Context) {
 	id := c.Param("id")
 	id1, _ := strconv.Atoi(id)
 	user, err := h.userUsecase.GetUserByID(int64(id1))
@@ -65,6 +66,79 @@ func (h *UserHandler) GetUserByID(c *gin.Context) {
 		Name:      user.Name,
 		Email:     user.Email,
 		CreatedAt: user.CreatedAt,
+	}
+
+	c.JSON(http.StatusOK, response)
+
+}
+
+// Versioning For New Client V2 (http://127.0.0.1:8080/api/v2/users)
+func (h *UserHandler) CreateUserV2(c *gin.Context) {
+	var request model.CreateUserRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user, err := h.userUsecase.CreateUser(request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// response := model.UserResponse{
+	// 	ID:    user.ID,
+	// 	Name:  user.Name,
+	// 	Email: user.Email,
+	// 	CreatedAt: user.CreatedAt,
+	// }
+
+	type Response struct {
+		ID    int64
+		Name  string
+		Email string
+	}
+	response := Response{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+
+	c.JSON(http.StatusCreated, response)
+
+}
+
+func (h *UserHandler) GetUserByIDV2(c *gin.Context) {
+	id := c.Param("id")
+	id1, _ := strconv.Atoi(id)
+	user, err := h.userUsecase.GetUserByID(int64(id1))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "user not found",
+		})
+		return
+	}
+
+	// response := model.UserResponse{
+	// 	ID:    user.ID,
+	// 	Name:  user.Name,
+	// 	Email: user.Email,
+	// 	CreatedAt: user.CreatedAt,
+	// }
+	type Response struct {
+		ID    int64
+		Name  string
+		Email string
+	}
+	response := Response{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
 	}
 
 	c.JSON(http.StatusOK, response)
--- user-service/internal/infrastructure/router.go
+++ user-service/internal/infrastructure/router.go
@@ -9,10 +9,18 @@ import (
 func NewRouter(userHandler *handler.UserHandler) *gin.Engine {
 	router := gin.Default()
 
-	users := router.Group("/api/users")
+	// For Old client Versioning
+	v1 := router.Group("/api/v1/users")
 	{
-		users.POST("", userHandler.CreateUser)
-		users.GET("/:id", userHandler.GetUserByID)
+		v1.POST("", userHandler.CreateUserV1)
+		v1.GET("/:id", userHandler.GetUserByIDV1)
+	}
+
+	// For New client Versioning
+	v2 := router.Group("/api/v2/users")
+	{
+		v2.POST("", userHandler.CreateUserV2)
+		v2.GET("/:id", userHandler.GetUserByIDV2)
 	}
 
 	return router
